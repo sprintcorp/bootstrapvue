53479e5080fefebc311cedfcb92002b08f8ce528
omohemma <emmaomoh@gmail.com>
2020-02-16 01:31:14 +0100

    Setup Project Structure, Header,Footer and Home page

---
+++ src/pages/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Header></Header>
+    <div style="height:65vh"></div>
+
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "../../shared/home/header/header.vue";
+import Footer from "../../shared/home/footer/footer.vue";
+export default {
+  name: "layout Page",
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  font-family: "Inter", Arial, "Helvetica Neue", "Segoe UI", sans-serif,
+    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #343a40;
+  text-align: left;
+  background-color: #fff;
+}
+</style>
---
+++ src/shared/home/footer/footer.vue
@@ -0,0 +1,21 @@
+<template>
+ 
+<!-- ========================= FOOTER ========================= -->
+<footer class="section-footer border-top padding-y">
+	<div class="container">
+		<p class="float-md-right"> 
+			&copy; Copyright 2019 All rights reserved
+		</p>
+		<p>
+			<a href="#">Terms and conditions</a>
+		</p>
+	</div><!-- //container -->
+</footer>
+<!-- ========================= FOOTER END // ========================= -->
+</template>
+
+<script src="./footer.js"></script>
+
+<style lang="scss" scoped>
+@import url("./footer.scss");
+</style>
